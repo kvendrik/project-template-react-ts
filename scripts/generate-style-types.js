705fcd5f6a22a59f4e2ef2c74891eae5b2ed6c33
Koen Vendrik <k.vendrik@gmail.com>
2018-04-18 14:19:40 +0200

    updates packages and cleans up webpack config

--- scripts/generate-style-types.js
+++ scripts/generate-style-types.js
@@ -1,9 +1,12 @@
+// More info here:
+// https://medium.com/@kvendrik/generating-typings-for-your-css-modules-in-webpack-2beb3739b342
+
 const DtsCreator = require('typed-css-modules');
 const glob = require('glob');
 
 const creator = new DtsCreator();
 
-glob('app/components/**/*.scss', {}, (error, filePaths) => {
+glob(process.argv[2], {}, (error, filePaths) => {
   for (const filePath of filePaths) {
     creator.create(filePath)
       .then(content => content.writeFile())
--- webpack.config.js
+++ webpack.config.js
@@ -4,103 +4,90 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 
-function configHelper(paths = {}, options = {}) {
-  const dirPath = __dirname;
+const PATHS = {
+  app: `${__dirname}/app`,
+  public: `${__dirname}/public`,
+};
 
-  const PATHS = {
-    app: paths.app || `${dirPath}/app`,
-    public: paths.public || `${dirPath}/public`,
-  };
-
-  let baseConfig = {
-    mode: process.env.PRODUCTION ? 'production' : 'development',
-    devServer: {
-      contentBase: PATHS.public,
-      port: 9000,
-      compress: true,
-      historyApiFallback: {
-        rewrites: [
-          { from: /^\/[^\.]+$/, to: '/index.html' },
+module.exports = {
+  mode: process.env.PRODUCTION ? 'production' : 'development',
+  devServer: {
+    contentBase: PATHS.public,
+    port: 9000,
+    compress: true,
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/[^\.]+$/, to: '/index.html' },
+      ],
+    },
+  },
+  devtool: 'cheap-module-eval-source-map',
+  entry: {
+    app: ['babel-polyfill', PATHS.app],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    modules: [
+      path.resolve('./node_modules'),
+    ],
+  },
+  output: {
+    path: PATHS.public,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index-template.html',
+    }),
+    new WebpackShellPlugin({
+      onBuildStart: ['yarn build:style-types app/components/**/*.scss'],
+    }),
+    new webpack.WatchIgnorePlugin([
+      /scss\.d\.ts$/
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg|otf)(\?.*$|$)/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              namedExport: true,
+              modules: true,
+              importLoaders: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./app/styles/modules'],
+            },
+          },
         ],
       },
-    },
-    devtool: 'cheap-module-eval-source-map',
-    entry: {
-      app: ['babel-polyfill', PATHS.app],
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
-      modules: [
-        path.resolve('./node_modules'),
-      ],
-    },
-    output: {
-      path: PATHS.public,
-      filename: 'bundle.js',
-      publicPath: '/',
-    },
-    plugins: [
-      new webpack.LoaderOptionsPlugin({
-        options: {
-          postcss: [autoprefixer()],
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /(node_modules)/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015'],
+          plugins: ['transform-decorators-legacy', 'transform-object-rest-spread']
         },
-      }),
-      new HtmlWebpackPlugin({
-        template: 'index-template.html',
-      }),
-      new WebpackShellPlugin({
-        onBuildStart: ['yarn build:style-types'],
-      }),
-      new webpack.WatchIgnorePlugin([
-        /scss\.d\.ts$/
-      ])
+      },
     ],
-    module: {
-      rules: [
-        {
-          test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg|otf)(\?.*$|$)/,
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]'
-          }
-        },
-        {
-          test: /\.scss$/,
-          loader: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-              query: {
-                namedExport: true,
-                modules: true,
-                importLoaders: true,
-                localIdentName: '[name]__[local]___[hash:base64:5]',
-              },
-            },
-            'postcss-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                includePaths: ['./app/styles/modules'],
-              },
-            },
-          ],
-        },
-        {
-          test: /\.tsx?$/,
-          loader: 'awesome-typescript-loader',
-          exclude: /(node_modules)/,
-          query: {
-            cacheDirectory: true,
-            presets: ['react', 'es2015'],
-            plugins: ['transform-decorators-legacy', 'transform-object-rest-spread']
-          },
-        },
-      ],
-    },
-  };
-
-  return baseConfig;
-}
-
-module.exports = configHelper();
+  },
+};
