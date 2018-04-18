79ddc6b3fa678319f64417acf4fb36dafb253884
Koen Vendrik <k.vendrik@gmail.com>
2018-04-18 11:39:14 +0200

    adds sass modules example and cleans up button component

--- app/components/Button/Button.test.tsx
+++ app/components/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {mount} from 'enzyme';
-import Button from '../';
+import Button from './';
 
 it('renders children', () => {
   const wrapper = mount(<Button>Click me!</Button>);
--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import * as styles from './Button.scss';
 
-interface Props {
+export interface Props {
   children: React.ReactNode;
 }
 
