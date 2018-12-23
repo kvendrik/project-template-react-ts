bc96ab2bb9f04bc500c7374b6d9899e83aa49a49
Koen Vendrik <k.vendrik@gmail.com>
2018-12-22 21:38:59 -0500

    updates tooling

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-
 import * as styles from './Button.scss';
 
 export interface Props {
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {render} from 'react-dom';
-
-import Button from 'Components/Button';
+import Button from 'components/Button';
 
 render(<Button>Click me!</Button>, document.getElementById('app'));
